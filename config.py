0fbacd79cd5d4304d7d34cf862a82b0ce9ed6f00
kartikey2807 <kartikey2807@gmail.com>
2025-10-03 16:18:54 +0200

    compute SIGMA

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 
@@ -13,6 +14,11 @@ CRITIC_ITER = 5
 CLIP = 0.01
 LEARNING_RATE_C = 0.0005
 LEARNING_RATE_G = 0.00005
-EPOCH = 30 ## x10
-EPSILON = 4
+EPOCH = 1 ## ^0^
+EPSILON = 0.001
+DELTA = 1e-5
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+## Formula taken from DPGAN article
+## https://arxiv.org/pdf/1802.06739
+## σ = 2q[√n_d*log(1/δ)]/ɛ
+SIGMA = 2*(BATCH_SIZE/60_000)*(math.sqrt(CRITIC_ITER*math.log10(1/DELTA))/EPSILON)
